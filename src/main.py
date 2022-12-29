ffee4c391ab5de9e7d4924671a4704cc87e53865
sabitov-kirill <sabitovkirill@ya.ru>
2022-12-29 22:03:36 +0300

    Initial preparation.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+
+def main():
+    bitmap = Image.new("RGB", (500, 500), "white")
+    # TODO: implement rendering
+    bitmap.save("mandel.jpg")
+
+
+if __name__ == main:
+    main()
